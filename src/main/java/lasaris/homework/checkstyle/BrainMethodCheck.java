b32ab9c50b6c3b7d33b60e6bb54f3231ef35cbf5
Michal Sladecek <469434@mail.muni.cz>
2021-05-28 13:56:20 +0200

    Added setting of parameters

--- src/main/java/lasaris/homework/checkstyle/BrainMethodCheck.java
+++ src/main/java/lasaris/homework/checkstyle/BrainMethodCheck.java
@@ -3,6 +3,28 @@ package lasaris.homework.checkstyle;
 import com.puppycrawl.tools.checkstyle.api.AbstractCheck;
 
 public class BrainMethodCheck extends AbstractCheck {
+    int maxLinesOfCode;
+    int maxCyclomaticComplexity;
+    int maxNesting;
+    int maxVariables;
+
+    public void setMaxLinesOfCode(int maxLinesOfCode) {
+        this.maxLinesOfCode = maxLinesOfCode;
+    }
+
+    public void setMaxCyclomaticComplexity(int maxCyclomaticComplexity) {
+        this.maxCyclomaticComplexity = maxCyclomaticComplexity;
+    }
+
+    public void setMaxNesting(int maxNesting) {
+        this.maxNesting = maxNesting;
+    }
+
+    public void setMaxVariables(int maxVariables) {
+        this.maxVariables = maxVariables;
+    }
+
+    
     @Override
     public int[] getDefaultTokens() {
         return new int[0];
