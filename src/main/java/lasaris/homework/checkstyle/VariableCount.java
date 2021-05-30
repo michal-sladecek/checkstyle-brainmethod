0c7118999de72042b662cb13dac16c6f0a8d7ed5
Michal Sladecek <469434@mail.muni.cz>
2021-05-30 12:31:55 +0200

    Added checking of variable number

---
+++ src/main/java/lasaris/homework/checkstyle/VariableCount.java
@@ -0,0 +1,30 @@
+package lasaris.homework.checkstyle;
+
+import com.puppycrawl.tools.checkstyle.api.DetailAST;
+import com.puppycrawl.tools.checkstyle.api.TokenTypes;
+
+public class VariableCount implements  MethodMetric{
+
+    int numberOfVariables = 0;
+    @Override
+    public int getMetric() {
+        return numberOfVariables;
+    }
+
+    @Override
+    public void visitMethodDefinition(DetailAST ast) {
+        return;
+    }
+
+    @Override
+    public void cleanupAfterMethod() {
+        numberOfVariables = 0;
+    }
+
+    @Override
+    public void visitToken(DetailAST ast) {
+        if(ast.getType() == TokenTypes.VARIABLE_DEF){
+            numberOfVariables++;
+        }
+    }
+}
